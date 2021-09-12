7a86e91a31ef8f35a0cbb5a83cbf9ff8d38dc8f2
AKA迪迪弄 <wl00494849@gmail.com>
2021-09-12 21:02:27 +0800

    fix

--- Assets/Script/GameDataModel/Damage.cs
+++ Assets/Script/GameDataModel/Damage.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 namespace GameDataModel
 {
-    public class Damage 
-{
-    public Vector3 origin;
-    public int damageAmount;
-    public float pushForce;
+    public class Damage
+    {
+        public Vector3 origin;
+        public int damageAmount;
+        public float pushForce;
 
-}
+    }
 }
 
